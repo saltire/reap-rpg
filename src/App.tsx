14d722835a243536a4f2b8ec215e416971313a3f
saltire sable <saltiresable@gmail.com>
2023-08-10 16:44:32 -0400

    Use reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,61 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useMemo, useReducer } from 'react';
 
 import points from './points';
 import SplitLines from './components/SplitLines';
-import { State, StateContext, initialState } from './state';
+import { reducer, GameStateContext, DispatchContext, initialState } from './state';
 import { exists } from './utils';
 
 
 export default function App() {
-  const [state, setState] = useState<State>(initialState);
+  const [game, dispatch] = useReducer(reducer, initialState);
 
-  const goToPoint = useCallback((pointId: number) => setState(prev => ({
-    ...prev,
-    pointId,
-    points: {
-      ...prev.points,
-      [pointId]: {
-        ...prev.points[pointId],
-        visited: true,
-      },
-    },
-  })), []);
+  const goToPoint = (pointId: number) => dispatch({ type: 'go_to_point', pointId });
 
-  const point = points.find(p => p.id === state.pointId);
+  const point = points.find(p => p.id === game.pointId);
   const exits = useMemo(
     () => (point?.exits ?? []).map(pid => points.find(p => p.id === pid)).filter(exists)
-      .map(exit => ({ ...exit, visited: state.points[exit.id]?.visited })),
-    [point, state.points]);
+      .map(exit => ({ ...exit, visited: game.points[exit.id]?.visited })),
+    [point, game.points]);
 
   if (!point) {
     return null;
   }
 
   return (
-    <StateContext.Provider value={state}>
-      <div className='App'>
-        <header className='mb-8 py-2 bg-black text-white text-center'>
-          <h1 className='font-header text-5xl'>REAP: Kyrie Realm</h1>
-        </header>
-        <main className='max-w-screen-sm mx-auto text-center'>
-          <h2 className='flex items-center justify-center my-8 font-bold'>
-            <span className='w-6 h-6 leading-6 mr-2 bg-black text-white text-xl rounded-full'>
-              {point.id}
-            </span>
-            <span className='text-3xl'>{point.name}</span>
-          </h2>
-          <div className='text-xl text-left'>
-            <SplitLines text={point.description} />
-          </div>
-          <p className='w-max mx-auto my-8'>
-            {exits.map(exit => (
-              <button
-                key={exit.id}
-                type='button'
-                className='flex items-center w-full mx-auto my-2 px-2 ring-1 ring-black rounded-sm hover:bg-zinc-300 cursor-pointer'
-                onClick={() => goToPoint(exit.id)}
-              >
-                <span className='w-4 h-4 leading-4 mr-1 bg-black text-white text-md rounded-full'>
-                  {exit.id}
-                </span>
-                <span className={!exit.visited ? 'font-black' : undefined}>{exit.name}</span>
-              </button>
-            ))}
-          </p>
-        </main>
-      </div>
-    </StateContext.Provider>
+    <GameStateContext.Provider value={game}>
+      <DispatchContext.Provider value={dispatch}>
+        <div className='App'>
+          <header className='mb-8 py-2 bg-black text-white text-center'>
+            <h1 className='font-header text-5xl'>REAP: Kyrie Realm</h1>
+          </header>
+          <main className='max-w-screen-sm mx-auto text-center'>
+            <h2 className='flex items-center justify-center my-8 font-bold'>
+              <span className='w-6 h-6 leading-6 mr-2 bg-black text-white text-xl rounded-full'>
+                {point.id}
+              </span>
+              <span className='text-3xl'>{point.name}</span>
+            </h2>
+            <div className='text-xl text-left'>
+              <SplitLines text={point.description} />
+            </div>
+            <p className='w-max mx-auto my-8'>
+              {exits.map(exit => (
+                <button
+                  key={exit.id}
+                  type='button'
+                  className='flex items-center w-full mx-auto my-2 px-2 ring-1 ring-black rounded-sm hover:bg-zinc-300 cursor-pointer'
+                  onClick={() => goToPoint(exit.id)}
+                >
+                  <span className='w-4 h-4 leading-4 mr-1 bg-black text-white text-md rounded-full'>
+                    {exit.id}
+                  </span>
+                  <span className={!exit.visited ? 'font-black' : undefined}>{exit.name}</span>
+                </button>
+              ))}
+            </p>
+          </main>
+        </div>
+      </DispatchContext.Provider>
+    </GameStateContext.Provider>
   );
 }
--- src/state.tsx
+++ src/state.tsx
@@ -1,14 +1,14 @@
-import { createContext } from 'react';
+import { createContext, Dispatch, Reducer, useContext } from 'react';
 
 
-export type State = {
+export type GameState = {
   pointId: number,
   points: Record<number, ({
     visited?: true,
   } | undefined)>,
 };
 
-export const initialState: State = {
+export const initialState: GameState = {
   pointId: 1,
   points: {
     1: {
@@ -17,4 +17,32 @@ export const initialState: State = {
   },
 };
 
-export const StateContext = createContext<State>(initialState);
+export type Action = {
+  type: 'go_to_point',
+  pointId: number,
+} | {
+  type: 'other',
+};
+
+export const reducer: Reducer<GameState, Action> = (game: GameState, action: Action) => {
+  if (action.type === 'go_to_point') {
+    return {
+      ...game,
+      pointId: action.pointId,
+      points: {
+        ...game.points,
+        [action.pointId]: {
+          ...game.points[action.pointId],
+          visited: true,
+        },
+      },
+    };
+  }
+  return game;
+};
+
+export const GameStateContext = createContext<GameState>(initialState);
+export const DispatchContext = createContext<Dispatch<Action>>(() => {});
+
+export const useGameState = () => useContext(GameStateContext);
+export const useDispatch = () => useContext(DispatchContext);
